e9f91822b464482fa14e562c2460d2472adbdfaf
a96lex <a96lex@gmail.com>
2024-03-18 10:21:47 +0100

    feat: add datepicker

--- lib/components/new_event_modal.dart
+++ lib/components/new_event_modal.dart
@@ -1,8 +1,10 @@
 import 'package:better/components/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:better/components/date_time.dart';
+import 'package:intl/intl.dart';
 
-class NewEntryModal extends StatelessWidget {
+class NewEntryModal extends StatefulWidget {
   final String text;
 
   const NewEntryModal({
@@ -11,8 +13,59 @@ class NewEntryModal extends StatelessWidget {
   });
 
   @override
+  NewEntryModalState createState() => NewEntryModalState();
+}
+
+class NewEntryModalState extends State<NewEntryModal> {
+  String defaultDateTime = "";
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    setState(() {
+      defaultDateTime = _getDefaultDateTime(context);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return CustomButton(onPressed: () => _dialogBuilder(context), text: text);
+    // Calculate defaultDateTime when the widget is built
+    return CustomButton(
+        onPressed: () => _dialogBuilder(context), text: widget.text);
+  }
+
+  String _getDefaultDateTime(BuildContext context) {
+    // Calculate the default date and time string
+    print(context.mounted);
+    print("\n\n\n");
+    String formattedDate =
+        DateFormat.yMd(Localizations.localeOf(context).languageCode)
+            .format(DateTime.now());
+    String formattedTime = DateFormat('H:mm').format(DateTime.now());
+    return '$formattedDate $formattedTime';
+  }
+
+  Future<void> _showDateTimePicker(BuildContext context) async {
+    DateTime? selectedDateTime = await showDateTimePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2050),
+    );
+
+    if (selectedDateTime != null && context.mounted) {
+      // Handle the selected date and time
+      String formattedDate =
+          DateFormat.yMd(Localizations.localeOf(context).languageCode)
+              .format(selectedDateTime);
+      String formattedTime = DateFormat('H:mm').format(selectedDateTime);
+      setState(() {
+        print("whi $formattedDate $formattedTime");
+        defaultDateTime = "$formattedDate $formattedTime";
+        print("fucking setstate as called");
+      });
+    }
   }
 
   Future<void> _dialogBuilder(BuildContext context) {
@@ -21,10 +74,23 @@ class NewEntryModal extends StatelessWidget {
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text(AppLocalizations.of(context)!.newEntryModalHeader),
-          content: Text(
-            AppLocalizations.of(context)!.newEntryModalText,
-            style: const TextStyle(fontSize: 16),
-          ),
+          content: Column(children: [
+            Text(
+              AppLocalizations.of(context)!.newEntryModalText,
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 40),
+            CustomButton(
+              onPressed: () => _showDateTimePicker(context),
+              text: defaultDateTime,
+            ),
+            Text(defaultDateTime),
+            const SizedBox(height: 80),
+            CustomButton(
+              onPressed: () {},
+              text: AppLocalizations.of(context)!.newEntryModalButton,
+            )
+          ]),
           actions: <Widget>[
             TextButton(
               style: TextButton.styleFrom(
