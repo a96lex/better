18c5eb7907183b57c594c6127c47d0e6dbd01664
a96lex <a96lex@gmail.com>
2024-03-29 11:29:17 +0100

    feat: display message if no events

--- lib/screens/history.dart
+++ lib/screens/history.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:better/database/database_helper.dart';
 import 'package:better/models/event.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
@@ -33,43 +34,52 @@ class HistoryScreenState extends State<HistoryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: eventList.length,
-      itemBuilder: (context, index) {
-        int reversedIndex = eventList.length - 1 - index;
-        return Column(children: [
-          Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 10, 0),
-                child: Container(
-                  width: 12,
-                  height: 12,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Theme.of(context).primaryColor,
+    if (eventList.isEmpty) {
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(60, 10, 60, 0),
+        child: Text(AppLocalizations.of(context)!.historyNoEvents,
+            style: const TextStyle(fontSize: 16)),
+      );
+    } else {
+      return Padding(
+          padding: const EdgeInsets.fromLTRB(60, 10, 60, 0),
+          child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: eventList.length,
+              itemBuilder: (context, index) {
+                int reversedIndex = eventList.length - 1 - index;
+                return Column(children: [
+                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                    Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                        child: Container(
+                          width: 12,
+                          height: 12,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        )),
+                    Text(
+                      DateFormat.yMMMMEEEEd(
+                              Localizations.localeOf(context).languageCode)
+                          .add_Hm()
+                          .format(eventList[reversedIndex].date),
+                      style: const TextStyle(fontWeight: FontWeight.w600),
+                    )
+                  ]),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(22, 10, 42, 40),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                        '${eventList[reversedIndex].text}',
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
                   ),
-                )),
-            Text(
-              DateFormat.yMMMMEEEEd(
-                      Localizations.localeOf(context).languageCode)
-                  .add_Hm()
-                  .format(eventList[reversedIndex].date),
-              style: const TextStyle(fontWeight: FontWeight.w600),
-            )
-          ]),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(42, 10, 42, 40),
-            child: SizedBox(
-              width: double.infinity,
-              child: Text(
-                '${eventList[reversedIndex].text}',
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-        ]);
-      },
-    );
+                ]);
+              }));
+    }
   }
 }
